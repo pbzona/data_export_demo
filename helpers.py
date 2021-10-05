e787274ff685f9361f9560bcf4f4a76ee725ed05
Phil Zona <pzona@launchdarkly.com>
2021-10-05 10:15:38 -0400

    Fix helpers so they actually work

--- helpers.py
+++ helpers.py
@@ -3,16 +3,16 @@ import random
 def did_user_register(variations):
     registration_chance = 15 # Percent chance the user registers
     
-    if variations.trial_duration == 7:
+    if variations["trial_duration"] == 7:
         registration_chance += 5
-    if variations.trial_duration == 14:
+    if variations["trial_duration"] == 14:
         registration_chance += 0 # baseline
-    if variations.trial_duration == 21:
+    if variations["trial_duration"] == 21:
         registration_chance += 4
-    if variations.trial_duration == 28:
+    if variations["trial_duration"] == 28:
         registration_chance -= 2
 
-    if variations.cta_button_color != "blue":
+    if variations["cta_button_color"] != "blue":
         registration_chance += 3
 
     if random.randint(0, 100) < registration_chance:
